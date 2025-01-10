f31c4ed0e40cefeac15473ae9d8a267fd68d5c5f
Pengziyu <smpss0859@gmail.com>
2025-01-10 13:59:27 +0800

    HW6

---
+++ Assets/fbx_Body_1211_3/Body_1211_3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body_1211_3 : MonoBehaviour
+{
+    public GameObject videoPlayer;
+
+    public void VideoStart2()
+    {
+        videoPlayer.SetActive(true);
+    }
+}
